72d71e49b625d3e8f5e1e26ff0440fdf3aa4a911
marthamaybennett <marthamaybennett123@gmail.com>
2022-12-15 16:46:47 +0000

    swatting the bugs

--- task2b/index.js
+++ task2b/index.js
@@ -1,10 +1,19 @@
+/*PLAN
+
+1. Read the code
+2. Console.logging any and every bit we can (avoid debug console at all costs!!)
+3. Debug console (last resort.)
+
+*/
+
 function add(numb1, numb2) {
-  let num1 = String(numb1);
-  return num1 + numb2;
+  return numb1 + numb2;
+  //DONE!
 }
 
 function multiply(num1, operator, num2) {
-  num1, operator, num2;
+  return num1 * num2;
+  // DONE!
 }
 
 function hasPets(hasPet, firstName, lastName) {
@@ -13,6 +22,7 @@ function hasPets(hasPet, firstName, lastName) {
   } else {
     return;
     firstName + " " + lastName + " " + "has no pets";
+    //another numpty. That semicolon is premature and there's no else - it's part of the 'if' function!
   }
 }
 
@@ -21,12 +31,14 @@ let myAccountNumbers = {
   account2: 144,
   account3: 12,
   account4: 1443,
+  // ok, Jose. We see what you've done here: you've set line 31 as a string. How sad.
 };
 
 function addsBalanceOfAccounts() {
   let firstAccount = myAccountNumbers[1];
   let secondAccount = myAccountNumbers["account3"];
   return firstAccount + secondAccount;
+  // What the hell are you doing?! We're not cross, we're just disapointed
 }
 
 function calculator(firstNumber, operator, secondNumber) {
@@ -50,6 +62,9 @@ function calculator(firstNumber, operator, secondNumber) {
     return;
     answer;
   }
+  // oh no, on line 46 we've got the operator as a string - hang on a tick - 
+  //THERE'S NO RETURN AND YOU'VE DONE THIS FOR ALL OF THEM!
+  //In order to stop this runn
 }
 
 //  Do not change this.
